3ea1d9b6cc37a91c750fce4f9008940bcff5aeda
Debasish Bordoloi <debasishbordoloi555@gmail.com>
2025-11-26 21:05:35 +0530

    feat: add genetic algo code

---
+++ genetic_algorithm.py
@@ -0,0 +1,29 @@
+import heapq
+from p2_timetable import P2TimeTable
+
+def genetically_enhance(tt_list: list, max_patience=1000) -> Timetable:
+    patience = max_patience
+    num_tts = len(tt_list)
+
+    # populate the heap initially
+    tt_heap = []
+    for tt in tt_list:
+        heapq.heappush(tt_heap, tt)
+    last_best_penalty = tt_heap[0].getScore()
+
+    while patience > 0:
+        temp_heap = []
+        for i in range(num_tts):
+            curr_tt = heapq.heappop(tt_heap)
+            new_tt = curr_tt.mutate()
+            heapq.heappush(temp_heap, curr_tt)
+            heapq.heappush(temp_heap, new_tt)
+            tt_heap.clear()
+            tt_heap = temp_heap
+        if (tt_heap[0].getScore() < last_best_penalty):
+            last_best_penalty = tt_heap[0].getScore()
+            patience = max_patience
+        patience -= 1
+    
+    return heapq.heappop(tt_heap)
+            
